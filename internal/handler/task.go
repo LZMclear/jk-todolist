80ff1aa17f5af7b11b9a206930961ffa1de32156
key <3135679861@qq.com>
2025-11-26 10:53:18 +0800

    feat：定义API接口

---
+++ internal/handler/task.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+)
+
+func ListTasks(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateTask(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetTask(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateTask(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeleteTask(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
--- internal/server/router.go
+++ internal/server/router.go
@@ -3,6 +3,7 @@ package server
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
+	"jk-todolist/internal/handler"
 )
 
 func NewRouter(db *sql.DB) *gin.Engine {
@@ -12,5 +13,22 @@ func NewRouter(db *sql.DB) *gin.Engine {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
+	r.Static("/static", "web")
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("web/index.html")
+	})
+
+	api := r.Group("/api")
+	{
+		t := api.Group("/tasks")
+		{
+			t.GET("/", handler.ListTasks(db))
+			t.POST("/", handler.CreateTask(db))
+			t.GET(":id", handler.GetTask(db))
+			t.PUT(":id", handler.UpdateTask(db))
+			t.DELETE(":id", handler.DeleteTask(db))
+		}
+	}
 	return r
 }
